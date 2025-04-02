e260fd4cd01cc9d65090c8655766410aee1729b5
Muhammad  Ramadiansyah <muhammad.ramadiansyah@finaccel.co>
2025-04-03 01:16:48 +0700

    remove configure

--- intura_ai/__init__.py
+++ intura_ai/__init__.py
@@ -1,12 +1,3 @@
-"""Intura AI package for LLM experimentation and production."""
-
 from .__version__ import __version__
-from intura_ai.client.config import configure
 
-# Make key functions available at package level
-def set_verbose(verbose: bool = True) -> None:
-    """Enable or disable verbose mode (debug logging)."""
-    configure(verbose=verbose)
-
-# Your other imports and code here
 __author__ = "Intura Developer"
